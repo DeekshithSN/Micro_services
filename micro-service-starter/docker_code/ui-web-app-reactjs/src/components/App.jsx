5f5f1df3835a0d7bd6a83e9dda903b78a00ad786
DeekshithSN <deekshithsn@gmail.com>
2022-11-19 08:44:17 +0530

    dockerfile

--- micro-service-starter/docker_code/ui-web-app-reactjs/src/components/App.jsx
+++ micro-service-starter/docker_code/ui-web-app-reactjs/src/components/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   const [displayData, setDisplayData] = useState({url:''})
 
   const handleApiCall = async (e) => {
-    const url = 'http://34.125.31.251:9999/'+e.target.name;
+    const url = 'http://34.125.12.109:9999/'+e.target.name;
     displayData.url=url
     let options = {}
       try {
